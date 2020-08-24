a09780bc77f9654c317bc11c3b014138c90461c3
ro0opf <34717634+ro0opf@users.noreply.github.com>
2020-08-24 11:00:01 +0900

    Updates

---
+++ precache-manifest.6b341daa0d3dcd5036479b35f64b60c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be489abb88afae57ad497e82f5081cb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "860f3b1521bd806951ca",
+    "url": "/portfolio/static/js/2.603f759d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.603f759d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c92bf61bac5b966b43ca",
+    "url": "/portfolio/static/js/main.5775d3f2.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
